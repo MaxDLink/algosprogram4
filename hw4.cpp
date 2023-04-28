e6f8f9598f03d25ec0317f17b823551a90d3b8ce
Max Link <max.durham.link@gmail.com>
2023-04-28 12:01:30 -0500

    made sure functions were outputting to console

--- hw4.cpp
+++ hw4.cpp
@@ -1,21 +1,23 @@
 #include <vector>
+#include <bits/stdc++.h>
+
 
 using namespace std;
 //random values are stuffed into these functions so that output is printed. These values should be changed. 
 //no late pass will be used for this assignment.
 //driver program should be indexed at zero. He will fix this? 
 
-pair< vector<float>, vector<int> >  WWWWW(vector<float> w, vector<float> p, int s, int t)
+std::pair<std::vector<float>, std::vector<int>> WWWWW(std::vector<float> w, std::vector<float> p, int s, int t)
 {
-    int j = w.size() - 1;
-    vector<float> E(j+1, 0);
-    vector<int> Q(j+1, 0);
-    E[1] = w[1]*p[1];
-    Q[1] = 0;
-    for (int i=2; i<=j; i++) {
+    int j = w.size();
+    std::vector<float> E(j, 0);
+    std::vector<int> Q(j, 0);
+    E[0] = w[0] * p[0];
+    Q[0] = 0;
+    for (int i = 1; i < j; i++) {
         float max_val = w[i];
-        for (int k=1; k<i; k++) {
-            float val = w[k] + (1-p[k])*E[k-1];
+        for (int k = 0; k < i; k++) {
+            float val = w[k] + (1 - p[k]) * E[k];
             if (val > max_val) {
                 max_val = val;
             }
@@ -25,62 +27,68 @@ pair< vector<float>, vector<int> >  WWWWW(vector<float> w, vector<float> p, int 
             Q[i] = 1;
         }
     }
-    return make_pair(E, Q);
-
+    return std::make_pair(E, Q);
 }
 
-pair< vector< vector<float> > , vector< vector<int> > >  WWWWW_1(vector<float> w, vector<float> p, int s, int t)
-{
- int n = w.size();
-    vector<vector<float> > dp(n + 1, vector<float>(t + 1, 0));
-    vector<vector<int> > prev(n + 1, vector<int>(t + 1, -1));
+vector<vector<int>> WWWWW_1(vector<float> w, vector<float> p, int s, int t) {
+    int n = w.size();
+    float selected_items_weight = 0;
 
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = s; j <= t; j++)
-        {
-            if (j >= w[i - 1])
-            {
+    vector<vector<float>> dp(n + 1, vector<float>(t + 1, 0));
+    vector<vector<int>> path(n + 1, vector<int>(t + 1, -1));
+    for (int i = 1; i <= n; i++) {
+        for (int j = s; j <= t; j++) {
+            if (j >= w[i - 1]) {
                 float val = dp[i - 1][j - w[i - 1]] + p[i - 1];
-                if (val > dp[i][j])
-                {
+                if (val > dp[i - 1][j]) {
                     dp[i][j] = val;
-                    prev[i][j] = j - w[i - 1];
+                    path[i][j] = i - 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                    path[i][j] = path[i - 1][j];
                 }
-            }
-            if (dp[i - 1][j] > dp[i][j])
-            {
+            } else {
                 dp[i][j] = dp[i - 1][j];
-                prev[i][j] = j;
+                path[i][j] = path[i - 1][j];
             }
         }
     }
+    vector<vector<float>> selected_items(2);
+    vector<vector<int>> selected_indices(2);
 
-    vector<int> items;
-    int j = t;
-    for (int i = n; i >= 1; i--)
-    {
-        if (prev[i][j] != j)
-        {
-            items.push_back(i - 1);
-            j = prev[i][j];
-        }
-    }
-    reverse(items.begin(), items.end());
-
-    // Resize the vectors to the actual size
-    vector<vector<float> > res_first(2, vector<float>(items.size()));
-    vector<vector<int> > res_second(2, vector<int>(items.size()));
     for (int k = 0; k <= 1; k++) {
-        for (int i = 0; i < items.size(); i++) {
-            res_first[k][i] = dp[k * (n + 1) / 2 + items[i] + 1][t];
-            res_second[k][i] = items[i];
+        for (int i = s; i <= t; i++) {
+            if (dp[n][i] > dp[n][selected_items_weight]) {
+                selected_items_weight = i;
+            }
         }
+
+        vector<float> weights;
+        vector<float> profits;
+        vector<int> indices;
+
+        for (int i = n; i >= 1; i--) {
+            if (path[i][selected_items_weight] != -1 && dp[i][selected_items_weight] > dp[i-1][selected_items_weight]) {
+                indices.push_back(i-1);
+                weights.push_back(w[i-1]);
+                profits.push_back(p[i-1]);
+                selected_items_weight -= w[i-1];
+            }
+        }
+
+        // Reverse the order of the vectors and add them to selected_items and selected_indices
+        reverse(weights.begin(), weights.end());
+        reverse(profits.begin(), profits.end());
+        reverse(indices.begin(), indices.end());
+        selected_items[k] = weights;
+        selected_items[k] = profits;
+        selected_indices[k] = indices;
     }
 
-    return make_pair(res_first, res_second);
-
+    return selected_indices;
 }
+
+
 
  pair< vector< vector<float> > , vector< vector<int> > >  WWWWW_2(vector<float> w, vector<float> p, int s, int t)
  {
